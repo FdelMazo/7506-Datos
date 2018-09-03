c528ec269e5e7b6f92fedf18d7b8eec0871b32b0
Federico del Mazo <federicodelmazo@hotmail.com>
2018-09-03 14:53:18 -0300

    Agrego info geografica

--- install.sh
+++ install.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 
-apt install curl
-curl -O https://repo.continuum.io/archive/Anaconda3-5.2.0-Linux-x86_64.sh
+wget -nc https://repo.continuum.io/archive/Anaconda3-5.2.0-Linux-x86_64.sh
 sha256sum Anaconda3-5.2.0-Linux-x86_64.sh
 bash Anaconda3-5.2.0-Linux-x86_64.sh
