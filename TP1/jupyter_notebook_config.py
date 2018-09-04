942ed816b0d5263e5101b3abf83669a6c9ece50b
Federico del Mazo <federicodelmazo@hotmail.com>
2018-09-04 01:18:31 -0300

    Probanding

--- TP1/jupyter_notebook_config.py
+++ TP1/jupyter_notebook_config.py
@@ -7,7 +7,7 @@ def post_save(model, os_path, contents_manager):
     if model['type'] != 'notebook':
         return # only do this for notebooks
     d, fname = os.path.split(os_path)
-    check_call(['jupyter', 'nbconvert', '--to', 'html', fname], cwd=d)
-
+    name, ext = os.path.splitext(fname)
+    check_call(['cp', fname, name+'.preview'+ext], cwd=d)
 
 c.FileContentsManager.post_save_hook = post_save
